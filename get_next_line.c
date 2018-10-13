f87156b1fcd923d011e89d3c4b5acf5d8ae4c0c5
Chaneze AROUA <caroua@e1r3p1.42.fr>
2018-10-13 20:04:49 +0200

    changement

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: caroua <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/01 10:32:49 by caroua            #+#    #+#             */
-/*   Updated: 2018/10/12 09:43:42 by caroua           ###   ########.fr       */
+/*   Updated: 2018/10/13 20:02:58 by caroua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,11 +82,10 @@ int						get_next_line(const int fd, char **line)
 	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))
 		return (-1);
 	curr = get_correct_file(&file, fd);
-	MALLCHECK((*line = ft_strnew(1)));
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
-		MALLCHECK((curr->content = ft_strjoin(curr->content, buf)));
+		curr->content = ft_strjoin(curr->content, buf);
 		if (ft_strchr(buf, '\n'))
 			break ;
 	}
